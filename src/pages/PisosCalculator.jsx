06a251e4879a1f29783f48b94f7b3cc9a63f88aa
AndersonAraujjo <andersonbatista2010@gmail.com>
2022-12-04 17:15:07 -0300

    C4 -   atualização na barra de seleção

--- src/pages/PisosCalculator.jsx
+++ src/pages/PisosCalculator.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
+import retangulo from "../assets/retangulo.png";
+import trianguloRetangulo from "../assets/trianguloRetangulo.png";
+import trianguloIsocelis from "../assets/trianguloIsocelis.png";
+import trapezio from "../assets/trapezio.png";
+import diagonal from "../assets/diagonal.png";
+import paralelo from "../assets/paralelo.png";
 
 function PisosCalculator() {
     
   
-
     const[compAmbiente, setCompAmbiente] = useState();
+    const[compAmbienteMenor, setCompAmbienteMenor] = useState();
     const[largAmbiente, setLargAmbiente] = useState();
+    const[altAmbiente, setAltAmbiente] = useState()
+    
     const[compPeca, setCompPeca] = useState();
     const[largPeca, setLargPeca] = useState();
     const[compPecaRodape, setCompPecaRodape] = useState();
@@ -13,50 +21,152 @@ function PisosCalculator() {
     const[perimetro, setPerimetro] = useState();
     const[aberturas, setAberturas] = useState();
     const[alturaRodape, setAlturaRodape] = useState();
-    const[resultado, setResultado] = useState(0);
-    const[operacao, setOperacao] = useState("Área Retangular(10%)");
-    const areaComodo = compAmbiente*largAmbiente
+    const[resultado, setResultado] = useState();
+    const[operacao, setOperacao] = useState();
+    const[operacaoArea, setOperacaoArea] = useState();
+    const[calculoArea, setCalculoArea] = useState();
+   
     const areaPiso = (compPeca*largPeca)/10000
-    const rodape = Math.ceil((perimetro-aberturas)/(((compPecaRodape/alturaRodape))*alturaPecaRodape/100))
+    const rodape = Math.ceil((perimetro-aberturas)/((Math.floor((alturaPecaRodape/alturaRodape)))*compPecaRodape/100))
+
+    
+    const formDinamico = () => {
+      if (document.getElementById('areaRetangular').checked)
+        return (document.getElementById('compAmbiente').style.display = 'block'), (document.getElementById('largAmbiente').style.display = 'block'),(document.getElementById('altAmbiente').style.display = 'none'), (document.getElementById('compAmbienteMenor').style.display = 'none');
+      else if (document.getElementById('areaTriangular01').checked)
+        return (document.getElementById('compAmbiente').style.display = 'block'), (document.getElementById('largAmbiente').style.display = 'block'),(document.getElementById('altAmbiente').style.display = 'block'), (document.getElementById('compAmbienteMenor').style.display = 'none');
+      else if (document.getElementById('areaTriangular02').checked)
+        return (document.getElementById('compAmbiente').style.display = 'none'), (document.getElementById('largAmbiente').style.display = 'block'),(document.getElementById('altAmbiente').style.display = 'block'), (document.getElementById('compAmbienteMenor').style.display = 'none');
+      else if (document.getElementById('areaTrapezoidal').checked)
+        return (document.getElementById('compAmbiente').style.display = 'block'), (document.getElementById('largAmbiente').style.display = 'none'),(document.getElementById('altAmbiente').style.display = 'block'), (document.getElementById('compAmbienteMenor').style.display = 'block');
+    }
+
+    useEffect(() => {
+      setResultado(formDinamico());
+    }, [compAmbiente,largAmbiente,altAmbiente,compAmbienteMenor,operacaoArea]);
+
+
+
+    let s = (parseFloat(compAmbiente) + parseFloat(largAmbiente) + parseFloat(altAmbiente)) / 2.0;
+
+   
+    const areaComodos = () => {
+      if (document.getElementById('areaRetangular').checked)
+        return parseFloat(compAmbiente*largAmbiente);
+     else if (document.getElementById('areaTriangular01').checked)
+       return parseFloat(Math.sqrt(((parseFloat(compAmbiente) + parseFloat(largAmbiente) + parseFloat(altAmbiente)) / 2.0) * (((parseFloat(compAmbiente) + parseFloat(largAmbiente) + parseFloat(altAmbiente)) / 2.0) - compAmbiente) * (((parseFloat(compAmbiente) + parseFloat(largAmbiente) + parseFloat(altAmbiente)) / 2.0) - largAmbiente) * (((parseFloat(compAmbiente) + parseFloat(largAmbiente) + parseFloat(altAmbiente)) / 2.0) - altAmbiente)));
+     else if (document.getElementById('areaTriangular02').checked)
+        return parseFloat((largAmbiente*altAmbiente)/2);
+     else if (document.getElementById('areaTrapezoidal').checked)
+        return parseFloat((((parseFloat(compAmbiente))+(parseFloat(compAmbienteMenor)))/2)*parseFloat(altAmbiente));
+    }
+     useEffect(() => {
+      setCalculoArea(areaComodos());
+    }, [compAmbiente,largAmbiente,altAmbiente,compAmbienteMenor,operacaoArea,]);
+
+    const area = parseFloat(calculoArea);
+
 
 
     const calcular = () => {
-      if (operacao==="Área Retangular(10%)")
-        return parseFloat(areaComodo*1.10);
-      else if (operacao==="Área Trapezoidal(20%)")
-        return parseFloat(areaComodo*1.20);
-      else if (operacao==="Área com muitos recortes(30%)")
-        return parseFloat(areaComodo*1.30);
+      if (document.getElementById('areaRetangular').checked && document.getElementById('assentamentoParalelo').checked)
+        return parseFloat(area*1.10);
+      else if (document.getElementById('areaRetangular').checked && document.getElementById('assentamentoDiagonal').checked)
+        return parseFloat(area*1.20);
+
+      if (document.getElementById('areaTriangular01').checked && document.getElementById('assentamentoParalelo').checked)
+        return parseFloat(area*1.10);
+      else if (document.getElementById('areaTriangular01').checked && document.getElementById('assentamentoDiagonal').checked)
+        return parseFloat(area*1.20);
+
+      if (document.getElementById('areaTriangular02').checked && document.getElementById('assentamentoParalelo').checked)
+        return parseFloat(area*1.10);
+      else if (document.getElementById('areaTriangular02').checked && document.getElementById('assentamentoDiagonal').checked)
+        return parseFloat(area*1.20);
+
+      if (document.getElementById('areaTrapezoidal').checked && document.getElementById('assentamentoParalelo').checked)
+        return parseFloat(area*1.10);
+      else if (document.getElementById('areaTrapezoidal').checked && document.getElementById('assentamentoDiagonal').checked)
+        return parseFloat(area*1.20);
     }
 
     useEffect(() => {
       setResultado(calcular());
-    }, [compAmbiente,largAmbiente,compPeca,largPeca,operacao]);
+    }, [compAmbiente,largAmbiente,altAmbiente,compAmbienteMenor,compPeca,largPeca,operacao]);
 
     const peças = resultado/(areaPiso);
-   
 
-    
 
     return (
-
-      
-
       <div className="flex flex-col">
 
-        <div class="flex items-center w-full h-20 text-blue-900 bg-blue-900 fixed" >
+        <div className="flex items-center w-full h-20 text-blue-900 bg-blue-900 fixed" >
           <h1 className="mx-auto font-rubik text-4xl text-white">Minha Obra</h1>
         </div>
         <div className="flex text-2xl text-blue-900 font-bold mt-32">
           <h1 className="mx-auto font-sans font-bold">Quantificação de Pisos</h1>
         </div>
         <div className="flex  flex-col text-base mt-12">
-          <h1 className="mx-auto px-12 text-left text-blue-900 font-bold font-sans text-l ">Para calcular a quantidade de pisos primeiramente é necessário medir o comprimento e a largura do ambiente para fazer o input dos dados solicidados.
+          <h1 className="mx-auto px-12 text-left text-blue-900 font-bold font-sans text-l">Para calcular a quantidade de pisos a serem assentados em determinado local, primeiramente é necessário que sejam retiradas as medidas do ambiente a ser revestido, para com isso fazer o input dos dados solicitados abaixo. Com as medidas em mãos escolher o tipo de ambiente de acordo com a semelhança com o seu local e após isso iserir suas medidas.
           </h1>
           <h1 className="mx-auto px-12 text-left text-blue-900 font-bold font-sans text-l ">Para encontrar as dimensões e metragem do piso consultar o vendedor ou identificar na própria caixa do material.
           </h1>
+          <h1 className="mx-auto px-12 text-left text-blue-900 font-bold font-sans text-l ">Muita atenção na hora de escolher a direção do assentamento dos pisos, pois isso impctará na quantidade adequada de peças.
+          </h1>
             
-        </div>
+          </div>
+
+            <div className="flex flex-col px-12 mt-12 text-center text-blue-900  text-base font-sans font-bold">
+              <div className="mx-auto px-12 flex flex-col text-center text-base font-sans mt-12 text-blue-900 ">
+            <h1 className="mx-auto px-12 flex flex-col text-base font-sans font-bold">Selecione o tipo de ambiente:</h1>
+            
+           
+          </div>
+         
+
+          <div onChange={(e) => setOperacaoArea(e.target.value)}
+            className=" w-full grid grid-cols-2 sm:grid-cols-4 gap-6 text-center py-8 px-auto sm:px-0 mx-auto text-blue-900 font-sans font-bold " >
+            <div>
+              
+              <input name="theradio" id="areaRetangular" type='radio' value="areaRetangular"/>
+              <label className="pl-2">Retangular</label>
+              <img 
+                src={retangulo} 
+                 alt="" className="w-40 mx-auto" 
+                />
+            </div>
+            <div>
+              <input name="theradio" id="areaTriangular01" type='radio'value="areaTriangular01"/>
+              <label className="pl-2">Triangular</label>
+              <img 
+                src={trianguloRetangulo} 
+                 alt="" className="w-40 mx-auto" 
+                />
+            </div>
+            <div>
+              <input name="theradio" id="areaTriangular02" type='radio' value="areaTriangular02"/>
+              <label className="pl-2">Triangular</label>
+              <img 
+                src={trianguloIsocelis} 
+                 alt="" className="w-40 mx-auto" 
+                />
+            </div>
+            <div>
+              <input name="theradio" id="areaTrapezoidal" type='radio' value="areaTrapezoidal"/>
+              <label className="pl-2">Trapezoidal</label>
+              <img 
+                src={trapezio} 
+                 alt="" className="w-40 mx-auto" 
+                />
+            </div>
+            
+          </div>
+            
+
+
+                
+            
+          </ div>
 
         <div className="mx-auto px-12 flex flex-col text-blue-900 text-base font-sans mt-12">
           <h1 className="mx-auto font-bold ">Insira o comprimento e largura do ambiente em m:</h1>
@@ -64,6 +174,7 @@ function PisosCalculator() {
             <div className="flex flex-col" >
               <input className=" my-1 px-2 text-blue-900 border border-blue-900 border-spacing-2 font-bold "
               placeholder="Comprimento (m)"
+              id="compAmbiente"
               type="number"
               value={compAmbiente}
               onChange={(e) => setCompAmbiente(e.target.value)}
@@ -72,13 +183,33 @@ function PisosCalculator() {
 
               <input className=" my-1 px-2 text-blue-900 border border-blue-900 border-spacing-1 font-bold"
               placeholder="Largura (m)"
+              id="largAmbiente"
               type="number"
               value={largAmbiente}
               onChange={(e) => setLargAmbiente(e.target.value)}
+              
               />
+                         
+              <input className=" my-1 px-2 text-blue-900 border border-blue-900 border-spacing-1 font-bold"
+              placeholder="Altura (m)"
+              id="altAmbiente"
+              type="number"
+              value={altAmbiente}
+              onChange={(e) => setAltAmbiente(e.target.value)}
+              
+              />
+              <input className=" my-1 px-2 text-blue-900 border border-blue-900 border-spacing-1 font-bold"
+              placeholder="Comprimento lado Menor (m)"
+              id="compAmbienteMenor"
+              type="number"
+              value={compAmbienteMenor}
+              onChange={(e) => setCompAmbienteMenor(e.target.value)}
+              
+              />
+
             </div>
         
-            <label>A área do ambiente é: {areaComodo.toFixed(2)}m²</label>
+            <label>A área do ambiente é: {area.toFixed(2)}m²</label>
           
         </div>
 
@@ -102,25 +233,41 @@ function PisosCalculator() {
               />
             </div>
         
-            <label>A área do seu Piso é : {areaPiso.toFixed(2)}m²</label>
+            <label>A área do seu Piso é : {areaPiso}m²</label>
         </div>
 
         <div className="mx-auto px-12 flex flex-col text-center text-base font-sans mt-12 text-blue-900 ">
-          <h1 className="mx-auto px-12 flex flex-col text-base font-sans font-bold">Porcentagem Perdas:</h1>
+          <h1 className="mx-auto px-12 flex flex-col text-base font-sans font-bold">Selecione a direção do assentamento:</h1>
+
+
           
-          <select onChange={(e) => setOperacao(e.target.value)}
-          className="mx-auto my-1 px-2 text-blue-900 border border-blue-900 font-sans font-bold " >
-            <option>Área Retangular(10%)</option>
-            <option>Área Trapezoidal(20%)</option>
-            <option>Área com muitos recortes(30%)</option>
-          </select>
-            <h1 className="mx-auto ">Selecione a opção considerando a geometria do ambiente:</h1>
-            <h3 className="mx-auto">Quanto mais angulado e desproporcional for o ambiente, maior será a porcentagem de perdas.</h3>
-          
+            <div onChange={(e) => setOperacao(e.target.value)}
+            className=" w-full grid grid-cols-2 gap-6 text-center py-8 px-auto sm:px-0 mx-auto text-blue-900 font-sans font-bold ">
+              
+              <div>
+                <input name="assentamento" id="assentamentoParalelo" type='radio' value="assentamentoParalelo"/>
+                <label className="pl-2">Paralelo</label>
+                <img 
+                  src={paralelo} 
+                  alt="" className="w-40 mx-auto" 
+                  />
+              </div>
+              <div>
+                <input name="assentamento" id="assentamentoDiagonal" type='radio' value="assentamentoDiagonal"/>
+                <label className="pl-2">Diagonal
+                </label>
+                <img 
+                  src={diagonal} 
+                  alt="" className="w-40 mx-auto" 
+                  />
+                </div>
+            </div>
+ 
+   
         </div>
 
-        <div className=" text-blue-900 mx-auto px-12 flex flex-col text-base font-sans font-bold mt-12">
-          <label>Você utilizará: {resultado.toFixed(2)}m² de piso ou {Math.round(peças)} peças de piso. </label>
+        <div className=" text-center text-blue-900 mx-auto px-12 flex flex-col text-base font-sans font-bold mt-12 border-2 border-blue-900">
+          <label >Você utilizará: {parseFloat(resultado).toFixed(2)}m² de piso ou {Math.round(peças)} peças de piso. </label>
         </div>
 
         <div>
@@ -176,7 +323,7 @@ function PisosCalculator() {
               Fique atento, no campo acima devem ser inseridos os comprimentos de paredes e aberturas em metros, as dimensões da peça em centimetros e a altura do rodapé também é em centimetros.
               </h2>
 
-              <div className="  text-blue-900 mx-auto px-12 flex flex-col text-base font-sans font-bold mb-12 mt-12">
+              <div className=" text-center text-blue-900 mx-auto px-12 flex flex-col text-base font-sans font-bold mb-12 mt-12 border-2 border-blue-900">
                 <label>Você utilizará: {(rodape*(compPecaRodape*alturaPecaRodape))/10000}m² ou {(rodape)} peças de piso para fazer o rodapé.</label>
               </div>
             </div>
